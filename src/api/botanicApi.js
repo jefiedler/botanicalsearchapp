884b6886d7c7a480342772d99534a6af583dc7ed
jefiedler <jonas.fiedler@magenta.de>
2020-08-17 20:21:35 +0200

    Refector object kategories

--- src/api/botanicApi.js
+++ src/api/botanicApi.js
@@ -2,15 +2,15 @@ export async function fetchApiData() {
   const api_Key = process.env.REACT_APP_BOTANICAL_API_KEY;
 
   const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token=${api_Key}&q=apple`
+    `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants/search?token=${api_Key}&q=lavandula`
   );
   if (!response.ok) {
     throw new Error(response);
   }
   const answer = await response.json();
   const plants = answer.data.map((plant) => ({
-    latName: plant.scientific_name,
-    normalName: plant.common_name,
+    species: plant.scientific_name,
+    name: plant.common_name,
     family: plant.family,
     genus: plant.genus,
     imgSrc: plant.image_url,
