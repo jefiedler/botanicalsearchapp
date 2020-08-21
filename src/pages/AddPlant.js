510d2183386b7f700f73d755637a39acdf537ec7
jefiedler <jonas.fiedler@magenta.de>
2020-08-21 16:19:45 +0200

    Finish post form

--- src/pages/AddPlant.js
+++ src/pages/AddPlant.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HeaderAddPlants from '../componens/headers/HeaderAddPlants';
-import { useMutation } from 'react-query';
 import { postPlants } from '../api/apiPlants';
 
 function AddPlant() {
@@ -9,25 +8,39 @@ function AddPlant() {
     const [germanName, setGermanName] = React.useState("");
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(false);
-    const [data, setData] = React.useState({});
-
-    // function handleSubmit(){
-    //     const [addPlant] = useMutation(postPlants);
-
-    // }
-
-
-
+    
+    async function handleSubmit(event){
+        event.preventDefault();
+        setLoading(true);
+        setError(false);
+        try {
+            await postPlants({
+                genus,
+                species,
+                germanName
+            });
+            setGenus("");
+            setSpecies("");
+            setGermanName("");
+        } catch (error) {
+            console.error(error);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    }
+    
 
     return (
         <>
             <HeaderAddPlants/>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label>Genus: <input type="text" value={genus} onChange={(event) => setGenus(event.target.value)}/></label>
                 <label>Species: <input type="text" value={species} onChange={(event)=> setSpecies(event.target.value)}/></label>
                 <label>German Name: <input type="text" value={germanName} onChange={(event)=> setGermanName(event.target.value)}/></label>
                 <input type="submit" value="New Plant" disabled={!genus || !species || !germanName || loading}/>
             </form>
+            {error && <p>Something bad happened. Please try again.</p>}
         </>
     );
 }
